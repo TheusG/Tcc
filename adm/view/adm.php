4c15c11bb09f30625441f53da3cacdf3c2158598
Theus <matheusgomeslucas@gmail.com>
2023-08-10 20:47:08 -0300

    adicionando tela do adm

---
+++ adm/view/adm.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/adm.css">
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img src="img/logo pizzaria1.png" alt="Logo">
+            <p>Five Stars</p>
+
+        </div>
+
+        <div class="dadosUsuario">
+            <br>
+            Nome: <br>
+            E-mail: <br>
+            Poder: <br>
+        </div>
+       
+     </header>
+     <div class="conteiner">
+        <div class="menu">
+            
+        </div>
+        <div class="ipres">
+            <iframe name="screen" id="screen" width="100%" height="1180px" src="bemvindo.php" style="border: 0px;"></iframe> 
+        </div>
+
+     </div>
+   
+</body>
+</html>
