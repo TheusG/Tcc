1aea6725d5b9c5baaa5a33226aeb550104766d3b
Felipe Cerqueira <133704759+LipeCerqueira@users.noreply.github.com>
2023-08-11 15:40:16 -0300

    Update db_fivestars.sql

--- db_fivestars.sql
+++ db_fivestars.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 10/08/2023 às 21:27
+-- Tempo de geração: 11/08/2023 às 20:39
 -- Versão do servidor: 10.4.28-MariaDB
 -- Versão do PHP: 8.2.4
 
