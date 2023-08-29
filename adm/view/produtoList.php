8517163580b42b0bd9b4b3ebe02dd73a3f5c273a
Felipe Cerqueira <133704759+LipeCerqueira@users.noreply.github.com>
2023-08-28 21:33:04 -0300

    Update produtoList.php

--- adm/view/produtoList.php
+++ adm/view/produtoList.php
@@ -8,7 +8,7 @@ require_once "admVerifSession.php";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Categorias</title>
+    <title>Produtos</title>
     <link rel="stylesheet" href="../view/css/adm.css">
     <script>
         function confirmDelete(id){
