2ebe440660d7605510813180b023bb23fd96aa3d
Theus <matheusgomeslucas@gmail.com>
2023-08-30 20:16:06 -0300

    arrumando produto

--- adm/view/produtoNew.php
+++ adm/view/produtoNew.php
@@ -17,32 +17,42 @@
         <br>
         <h3>Produtos da Pizzaria</h3>
         <h4>Novo Produto</h4>
+        <br>
     </div>
 
     <div id="admForm"><!-- esta no adm.css -->
         <form action="../controller/controller.php" method="post" name="produtoNew">
             <input type="hidden" name="produto_new" value="1">            
-               
+                <div class="divFlex">
+                    <div>
             <label for="nome">Nome do Produto</label><br>
-            <input class="inputnome" type="text" name="nome" value="" required ><br>
-                   
-            <label for="codigo">Código do Produto</label><br>
-            <input class="inputEmail" type="number" name="codigo" value=""><br><br>
-                   
-            <label for="descricao">Descrição do Produto</label><br>
-            <textarea name="descricao" id="descricao" cols="30" rows="10" style="resize: none;" required></textarea>
-                    
+            <input class="inputProduto" type="text" name="nome" value="" required ><br>
+                    </div>
+                    <div>
+            <label for="codigo">Código</label><br>
+            <input class="inputCod" type="number" name="codigo" value=""><br><br>
+                    </div>
+                    <div>
+                    <label for="valor">Valor</label><br>
+                    <input class="inputValor" type="number" step="0.01" name="valor" value="" required><br>
+                    </div>
+                </div>   
+            
+                    <div class="divFlex">
+                        <div class="estoque">
             <label for="estoque">Estoque</label><br>
             <input class="inputnome" type="number" name="estoque" value=""><br>
-                    
+                        </div>
+                        <div class="estoque">
             <label for="estoque_Min">Estoque Mínimo</label><br>
             <input class="inputnome" type="number" name="estoque_Min" value=""><br>
-                    
+                        </div>
+                        <div class="estoque">
             <label for="estoque_Max">Estoque Máximo</label><br>
             <input class="inputnome" type="number" name="estoque_Max" value=""><br>
-                   
-            <label for="valor">Valor</label><br>
-            <input class="inputnome" type="number" step="0.01" name="valor" value="" required><br>
+                            </div>
+                    </div>
+         
                    
             <label for="status">Status</label><br>
             <select name="status" id="status">
@@ -51,7 +61,7 @@
             </select>
                    
             <label for="nome">Imagem</label><br>
-            <input class="inputnome" type="file" name="imagem" value=""><br>
+           
                     
             <label for="categoria">Categoria</label><br>
             <select name="categoria" id="categoria">
@@ -61,7 +71,11 @@
                 <option value="4">Esfiha Doce</option>
                 <option value="5">Bebida</option>
             </select>
-                    
+            
+            <label for="descricao">Descrição do Produto</label><br>
+            <textarea name="descricao" id="descricao" cols="30" rows="10" style="resize: none;" required></textarea><br>
+           
+            <input class="inputnome" type="file" name="imagem" value=""><br>
             <input type="submit" name="sbmt" value="Enviar" ><br><br>
         </form>  
         <button class="voltar" id="btnVoltar" onclick="voltar();">Voltar</button>
