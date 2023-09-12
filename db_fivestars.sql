b38e6e3ff3953524cbe4da93d11ee894dd7be617
Felipe Cerqueira <133704759+LipeCerqueira@users.noreply.github.com>
2023-09-12 15:57:35 -0300

    Update db_fivestars.sql

--- db_fivestars.sql
+++ db_fivestars.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 08/09/2023 às 02:04
+-- Tempo de geração: 12/09/2023 às 20:56
 -- Versão do servidor: 10.4.28-MariaDB
 -- Versão do PHP: 8.2.4
 
@@ -196,8 +196,8 @@ INSERT INTO `perfil` (`Id_Perfil`, `Acessa_Cliente`, `Acessa_Funcionario`, `Aces
 CREATE TABLE `produto` (
   `Id_Produto` int(11) NOT NULL,
   `Cod_Produto` varchar(13) DEFAULT NULL,
-  `Nome_Produto` varchar(25) DEFAULT NULL,
-  `Desc_Produto` varchar(90) DEFAULT NULL,
+  `Nome_Produto` varchar(35) DEFAULT NULL,
+  `Desc_Produto` varchar(100) DEFAULT NULL,
   `Estoque` int(11) DEFAULT NULL,
   `Estoque_Min` int(11) DEFAULT NULL,
   `Estoque_Max` int(11) DEFAULT NULL,
@@ -229,7 +229,7 @@ INSERT INTO `produto` (`Id_Produto`, `Cod_Produto`, `Nome_Produto`, `Desc_Produt
 (15, '1015', 'Pizza Dois Queijos  ', 'Mussarela e catupiry                                                ', 0, 0, 0, 37.99, '1', 'pizzaDoisQueijos.jpg         ', 1),
 (16, '1016', 'Pizza Escarola      ', 'Escarola, mussarela e bacon                                         ', 0, 0, 0, 29.99, '1', 'pizzaEscarola.png            ', 1),
 (17, '1017', 'Pizza Frango        ', 'Frango desfiado temperado e cheddar                                 ', 0, 0, 0, 35, '1', 'pizzaFrango.png              ', 1),
-(18, '1018', 'Pizza Frango com Cat', 'Frango desfiado temperado com catupiry                              ', 0, 0, 0, 41.99, '1', 'pizzaFrangoCatupiry.png      ', 1),
+(18, '1018', 'Pizza Frango com Catupiry', 'Frango desfiado temperado com catupiry                              ', 0, 0, 0, 41.99, '1', 'pizzaFrangoCatupiry.png      ', 1),
 (19, '1019', 'Pizza Italiana      ', 'Calabresa, Salame, Pimentão Verde e Azeitonas Pretas Fatiadas       ', 0, 0, 0, 35.99, '1', 'pizzaItaliana.png            ', 1),
 (20, '1020', 'Pizza Lombo         ', 'Lombo fatiado, mussarela e bacon                                    ', 0, 0, 0, 41.99, '1', 'pizzaLombo.png               ', 1),
 (21, '1021', 'Pizza Marguerita    ', 'Mussarela, parmesão e manjericão                                    ', 0, 0, 0, 41.99, '1', 'pizzaMarguerita.png          ', 1),
@@ -242,46 +242,45 @@ INSERT INTO `produto` (`Id_Produto`, `Cod_Produto`, `Nome_Produto`, `Desc_Produt
 (28, '1028', 'Pizza Portuguesa ', 'Mussarela, presunto, ovo cozido e cebola ', 0, 0, 0, 39.99, '1', 'pizzaPortuguesa.png ', 1),
 (29, '1029', 'Pizza Toscana ', 'Calabresa moída e mussarela                                         ', 0, 0, 0, 29.99, '1', 'pizzaToscana.jpeg', 1),
 (30, '1030', 'Pizza Vegetariana ', 'Mussarela, tomates, champignon, pimentão, cebola e azeitonas verdes ', 0, 0, 0, 54.99, '1', 'pizzaVegetariana.jpg', 1),
-(31, '1031', 'Pizza Banana        ', 'Banana, leite condesado e canela em pó                              ', 0, 0, 0, 42.99, '1', 'pizzaBanana.png              ', 2),
-(32, '1032', 'Pizza Brigadeiro    ', 'Chocolate ao leite e chocolate granulado                            ', 0, 0, 0, 44.99, '1', 'pizzaBrigadeiro.png          ', 2),
-(33, '1033', 'Pizza Romeu e Juliet', 'Goiabada e mussarela                                                ', 0, 0, 0, 44.99, '1', 'pizzaRomeuJulieta.png        ', 2),
-(34, '1034', 'Pizza Sensação      ', 'Morango, chocolate e leite condesado                                ', 0, 0, 0, 35, '1', 'pizzaSensacao.png            ', 2),
-(35, '1035', 'Pizza Prestígio     ', 'Chocolate, coco ralado e leite condesado                            ', 0, 0, 0, 35, '1', 'pizzaPrestigio.png           ', 2),
-(36, '1036', 'Pizza BIS           ', 'Chocolate e Bis                                                     ', 0, 0, 0, 35, '1', 'pizzaBIS.jpg                 ', 2),
-(37, '1037', 'Pizza M&M´s         ', 'Chocolate M&M´s                                                     ', 0, 0, 0, 35, '1', 'pizzaMM.png                  ', 2),
-(38, '1038', 'Esfiha Atum         ', 'Atum                                                                ', 0, 0, 0, 4, '1', 'esfihaAtum.png               ', 3),
-(39, '1039', 'Esfiha Atum com Quei', 'Atum com queijo                                                     ', 0, 0, 0, 4, '1', 'esfihaAtumQueijo.png         ', 3),
-(40, '1040', 'Esfiha Calabresa com', 'Calabresa moída e catupity                                          ', 0, 0, 0, 4, '1', 'espihaCalabresaCatupiry.png  ', 3),
-(41, '1041', 'Esfiha Calabresa com', 'Calabresa moída e queijo                                            ', 0, 0, 0, 4, '1', 'espihaCalabresaQueijo.png    ', 3),
-(42, '1042', 'Esfiha Carne        ', 'Carne moída                                                         ', 0, 0, 0, 4, '1', 'espihaCarne.jpg              ', 3),
-(43, '1043', 'Esfiha Escarola e Qu', 'Escarola e queijo mussarela                                         ', 0, 0, 0, 4, '1', 'espihaEscarolaQueijo.png     ', 3),
-(44, '1044', 'Esfiha Frango       ', 'Frango desfiado                                                     ', 0, 0, 0, 4, '1', 'esfihaFrango.png             ', 3),
-(45, '1045', 'Esfiha Frango  com C', 'Frango desfiado e catupiry                                          ', 0, 0, 0, 4, '1', 'espihaFrangoCatupiry.png     ', 3),
-(46, '1046', 'Esfiha Queijo       ', 'Queijo mussarela                                                    ', 0, 0, 0, 4, '1', 'espihaQueijo.jpg             ', 3),
-(47, '1047', 'Esfiha Banana       ', 'Banana, leite condesado e canela em pó                              ', 0, 0, 0, 5, '1', 'espihaBanana.png             ', 4),
-(48, '1048', 'Esfiha Brigadeiro   ', 'Chocolate e granulado                                               ', 0, 0, 0, 5, '1', 'espihaBrigadeiro.jpg         ', 4),
-(49, '1049', 'Esfiha Chocolate com', 'Chocolate e m&m´s                                                   ', 0, 0, 0, 5, '1', 'espihaMM.png                 ', 4),
-(50, '1050', 'Esfiha Prestígio    ', 'Calabresa moída e queijo                                            ', 0, 0, 0, 5, '1', 'espihaCalabresaQueijo.png    ', 4),
-(51, '1051', 'Esfiha Carne        ', 'Chocolate, coco ralado e leite condesado                            ', 0, 0, 0, 5, '1', 'espihaPrestigio.png          ', 4),
-(52, '1052', 'Esfiha Romeu e Julie', 'Goiabada e mussarela                                                ', 0, 0, 0, 5, '1', 'espihaRomeuJulieta.jpg       ', 4),
-(53, '1053', 'Coca-Cola 350ml     ', 'Coca-Cola 350ml                                                     ', 0, 0, 0, 9, '1', 'Coca350ml.jpg                ', 5),
-(54, '1054', 'Coca-Cola 2 litros  ', 'Coca-Cola 2 litros                                                  ', 0, 0, 0, 15, '1', 'Coca2litros.png              ', 5),
-(55, '1055', 'Coca-Cola Zero 350ml', 'Coca-Cola Zero 350ml                                                ', 0, 0, 0, 9, '1', 'Cocazero350ml.png            ', 5),
-(56, '1056', 'Coca-Cola  Zero 2 Li', 'Coca-Cola  Zero 2 Litros                                            ', 0, 0, 0, 15, '1', 'Cocazero2litros.png          ', 5),
-(57, '1057', 'Fanta Laranja 350 ml', 'Fanta Laranja 350 ml                                                ', 0, 0, 0, 9, '1', 'Fantalaranja350ml.jpg        ', 5),
-(58, '1058', 'Fanta Laranja 2 litr', 'Fanta Laranja 2 litros                                              ', 0, 0, 0, 15, '1', 'Fanta2litros.png             ', 5),
-(59, '1059', 'Fanta Uva 350ml     ', 'Fanta Uva 350 ml                                                    ', 0, 0, 0, 9, '1', 'FantaUva350ml.png            ', 5),
-(60, '1060', 'Fanta Uva 2 litros  ', 'Fanta Uva 2 litros                                                  ', 0, 0, 0, 15, '1', 'FantaUva2litros.png          ', 5),
-(61, '1061', 'Guaraná Antartica 35', 'Guaraná Antartica 350ml                                             ', 0, 0, 0, 9, '1', 'Guarana350ml.jpg             ', 5),
-(62, '1062', 'Guaraná Antartica 2 ', 'Guaraná Antartica 2 litros                                          ', 0, 0, 0, 15, '1', 'Guarana2litros.jpg           ', 5),
-(63, '1063', 'Sprite 350ml        ', 'Sprite 350ml                                                        ', 0, 0, 0, 9, '1', 'Sprite350ml.png              ', 5),
-(64, '1064', 'Sprite 2 litros     ', 'Sprite 2 litros                                                     ', 0, 0, 0, 15, '1', 'sprite2litros.png            ', 5),
-(65, '1065', 'Pepsi 350ml         ', 'Pepsi 350ml                                                         ', 0, 0, 0, 9, '1', 'pepsi350ml.jpeg              ', 5),
-(66, '1066', 'Pepsi 2 litros      ', 'Pepsi 2 litros                                                      ', 0, 0, 0, 15, '1', 'Pepsi2litros.jpg             ', 5),
-(67, '1067', 'Pepsi Zero 350ml    ', 'Pepsi Zero 350ml                                                    ', 0, 0, 0, 9, '1', 'PepsiZero.png                ', 5),
-(68, '1068', 'Água Mineral 350ml  ', 'Água Mineral 350ml                                                  ', 0, 0, 0, 5, '1', 'aguaMineral.jpg              ', 5),
-(69, '1069', 'Água com Gás 350ml  ', 'Água com Gás 350ml                                                  ', 0, 0, 0, 6, '1', 'AguacomGas.png               ', 5),
-(70, '1070', 'Água Tônica Schweppe', 'Água Tônica Schweppes 350ml                                         ', 0, 0, 0, 7, '1', 'AguaTonica.jpg               ', 5);
+(31, '2031', 'Pizza Banana        ', 'Banana, leite condesado e canela em pó                              ', 0, 0, 0, 42.99, '1', 'pizzaBanana.png              ', 2),
+(32, '2032', 'Pizza Brigadeiro    ', 'Chocolate ao leite e chocolate granulado                            ', 0, 0, 0, 44.99, '1', 'pizzaBrigadeiro.png          ', 2),
+(33, '2033', 'Pizza Romeu e Julieta', 'Goiabada e mussarela                                                ', 0, 0, 0, 44.99, '1', 'pizzaRomeuJulieta.png        ', 2),
+(34, '2034', 'Pizza Sensação      ', 'Morango, chocolate e leite condesado                                ', 0, 0, 0, 35, '1', 'pizzaSensacao.png            ', 2),
+(35, '2035', 'Pizza Prestígio     ', 'Chocolate, coco ralado e leite condesado                            ', 0, 0, 0, 35, '1', 'pizzaPrestigio.png           ', 2),
+(36, '2036', 'Pizza BIS           ', 'Chocolate e Bis                                                     ', 0, 0, 0, 35, '1', 'pizzaBIS.jpg                 ', 2),
+(37, '2037', 'Pizza M&M´s         ', 'Chocolate M&M´s                                                     ', 0, 0, 0, 35, '1', 'pizzaMM.png                  ', 2),
+(38, '3038', 'Esfiha Atum         ', 'Atum                                                                ', 0, 0, 0, 4, '1', 'esfihaAtum.png               ', 3),
+(39, '3039', 'Esfiha Atum com Queijo', 'Atum com queijo                                                     ', 0, 0, 0, 4, '1', 'esfihaAtumQueijo.png         ', 3),
+(40, '3040', 'Esfiha Calabresa com Catupiry', 'Calabresa moída e catupity                                          ', 0, 0, 0, 4, '1', 'espihaCalabresaCatupiry.png  ', 3),
+(41, '3041', 'Esfiha Calabresa com Queijo', 'Calabresa moída e queijo                                            ', 0, 0, 0, 4, '1', 'espihaCalabresaQueijo.png    ', 3),
+(42, '3042', 'Esfiha Carne        ', 'Carne moída                                                         ', 0, 0, 0, 4, '1', 'espihaCarne.jpg              ', 3),
+(43, '3043', 'Esfiha Escarola e Queijo', 'Escarola e queijo mussarela                                         ', 0, 0, 0, 4, '1', 'espihaEscarolaQueijo.png     ', 3),
+(44, '3044', 'Esfiha Frango       ', 'Frango desfiado                                                     ', 0, 0, 0, 4, '1', 'esfihaFrango.png             ', 3),
+(45, '3045', 'Esfiha Frango  com Catupiry', 'Frango desfiado e catupiry                                          ', 0, 0, 0, 4, '1', 'espihaFrangoCatupiry.png     ', 3),
+(46, '3046', 'Esfiha Queijo       ', 'Queijo mussarela                                                    ', 0, 0, 0, 4, '1', 'espihaQueijo.jpg             ', 3),
+(47, '4047', 'Esfiha Banana       ', 'Banana, leite condesado e canela em pó                              ', 0, 0, 0, 5, '1', 'espihaBanana.png             ', 4),
+(48, '4048', 'Esfiha Brigadeiro   ', 'Chocolate e granulado                                               ', 0, 0, 0, 5, '1', 'espihaBrigadeiro.jpg         ', 4),
+(49, '4049', 'Esfiha Chocolate com M&M´s', 'Chocolate e m&m´s                                                   ', 0, 0, 0, 5, '1', 'espihaMM.png                 ', 4),
+(51, '4051', 'Esfiha Prestígio    ', 'Chocolate, coco ralado e leite condesado                            ', 0, 0, 0, 5, '1', 'espihaPrestigio.png          ', 4),
+(52, '4052', 'Esfiha Romeu e Julieta', 'Goiabada e mussarela                                                ', 0, 0, 0, 5, '1', 'espihaRomeuJulieta.jpg       ', 4),
+(53, '5053', 'Coca-Cola 350ml     ', 'Coca-Cola 350ml                                                     ', 0, 0, 0, 9, '1', 'Coca350ml.jpg                ', 5),
+(54, '5054', 'Coca-Cola 2 litros  ', 'Coca-Cola 2 litros                                                  ', 0, 0, 0, 15, '1', 'Coca2litros.png              ', 5),
+(55, '5055', 'Coca-Cola Zero 350ml', 'Coca-Cola Zero 350ml                                                ', 0, 0, 0, 9, '1', 'Cocazero350ml.png            ', 5),
+(56, '5056', 'Coca-Cola  Zero 2 litros', 'Coca-Cola  Zero 2 Litros                                            ', 0, 0, 0, 15, '1', 'Cocazero2litros.png          ', 5),
+(57, '5057', 'Fanta Laranja 350 ml', 'Fanta Laranja 350 ml                                                ', 0, 0, 0, 9, '1', 'Fantalaranja350ml.jpg        ', 5),
+(58, '5058', 'Fanta Laranja 2 litros', 'Fanta Laranja 2 litros                                              ', 0, 0, 0, 15, '1', 'Fanta2litros.png             ', 5),
+(59, '5059', 'Fanta Uva 350ml     ', 'Fanta Uva 350 ml                                                    ', 0, 0, 0, 9, '1', 'FantaUva350ml.png            ', 5),
+(60, '5060', 'Fanta Uva 2 litros  ', 'Fanta Uva 2 litros                                                  ', 0, 0, 0, 15, '1', 'FantaUva2litros.png          ', 5),
+(61, '5061', 'Guaraná Antartica 350ml ', 'Guaraná Antartica 350ml                                             ', 0, 0, 0, 9, '1', 'Guarana350ml.jpg             ', 5),
+(62, '5062', 'Guaraná Antártica 2 litros     ', 'Guaraná Antártica 2 litros                                          ', 0, 0, 0, 15, '1', 'Guarana2litros.jpg           ', 5),
+(63, '5063', 'Sprite 350ml        ', 'Sprite 350ml                                                        ', 0, 0, 0, 9, '1', 'Sprite350ml.png              ', 5),
+(64, '5064', 'Sprite 2 litros     ', 'Sprite 2 litros                                                     ', 0, 0, 0, 15, '1', 'sprite2litros.png            ', 5),
+(65, '5065', 'Pepsi 350ml         ', 'Pepsi 350ml                                                         ', 0, 0, 0, 9, '1', 'pepsi350ml.jpeg              ', 5),
+(66, '5066', 'Pepsi 2 litros      ', 'Pepsi 2 litros                                                      ', 0, 0, 0, 15, '1', 'Pepsi2litros.jpg             ', 5),
+(67, '5067', 'Pepsi Zero 350ml    ', 'Pepsi Zero 350ml                                                    ', 0, 0, 0, 9, '1', 'PepsiZero.png                ', 5),
+(68, '5068', 'Água Mineral 350ml  ', 'Água Mineral 350ml                                                  ', 0, 0, 0, 5, '1', 'aguaMineral.jpg              ', 5),
+(69, '5069', 'Água com Gás 350ml  ', 'Água com Gás 350ml                                                  ', 0, 0, 0, 6, '1', 'AguacomGas.png               ', 5),
+(70, '5070', 'Água Tônica Schweppes 350ml       ', 'Água Tônica Schweppes 350ml                                         ', 0, 0, 0, 7, '1', 'AguaTonica.jpg               ', 5);
 
 -- --------------------------------------------------------
 
@@ -309,7 +308,7 @@ CREATE TABLE `usuario` (
 
 INSERT INTO `usuario` (`Id_Usuario`, `Nome_Usuario`, `Senha`, `Sexo`, `Cep`, `Numero`, `Complemento`, `Telefone`, `Email`, `Nascimento`, `Foto`) VALUES
 (1, 'Felipe Cerqueira', '5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5', 'm', 1, '400', 'Escola', '(11) 99593-7887', 'felipe@pizza.com', '2005-09-02', 'semfoto.jpg'),
-(15, 'Ingrid Passos', '5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5', 'f', 1, '400', 'Escola', '(11) 11111-1111', 'ingrid@pizza.com', '2023-09-06', 'semfoto.jpg'),
+(15, 'Ingrid Passos', '5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5', 'f', 1, '400', 'Escola', '(11) 11111-1111', 'ingrid@pizza.com', '2003-02-14', 'semfoto.jpg'),
 (20, 'Pedro Henrique', '5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5', 'n', 1, '9', 'Escola', '(11) 11111-1111', 'pedro@pizza.com', '2023-09-07', NULL);
 
 --
@@ -393,7 +392,7 @@ ALTER TABLE `cargo`
 -- AUTO_INCREMENT de tabela `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `Id_Categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `Id_Categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de tabela `cep`
@@ -429,7 +428,7 @@ ALTER TABLE `perfil`
 -- AUTO_INCREMENT de tabela `produto`
 --
 ALTER TABLE `produto`
-  MODIFY `Id_Produto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=72;
+  MODIFY `Id_Produto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=116;
 
 --
 -- AUTO_INCREMENT de tabela `usuario`
