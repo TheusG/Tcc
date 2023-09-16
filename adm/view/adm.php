dd2f7caa54f02624abc084ea35c8e0ce7f545af2
Felipe Cerqueira <133704759+LipeCerqueira@users.noreply.github.com>
2023-09-16 20:54:19 -0300

    nome vim do banco de dados

--- adm/view/adm.php
+++ adm/view/adm.php
@@ -1,9 +1,10 @@
-<?php 
+<?php
 require_once "admVerifSession.php";
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -12,43 +13,51 @@ require_once "admVerifSession.php";
     <link rel="stylesheet" href="css/adm.css">
     <script src="../libs/jquery.min.js"></script>
     <script>
-        function ExecutaLogout(){
+        function ExecutaLogout() {
             var resp = confirm('Deseja sair?');
-            if(resp==true){
-                location.href="admLogout.php";
-            }else{
+            if (resp == true) {
+                location.href = "admLogout.php";
+            } else {
                 return null;
             }
         }
     </script>
 </head>
+
 <body>
     <header>
         <div class="logo">
 
-            
-    
+
+
             <img src="img/logo pizzaria1.png" alt="Logo">
-            
-               
-            
-            <p>Five Stars</p>
+
+            <?php
+            require_once "../model/Empresa.class.php";
+            $empresa = new Empresa();
+            $info = $empresa->infoEmpresa();
+ 
+           
+            ?>
+            <p><?php for ($i = 0; $i < count($info); $i++){
+                echo $info[$i]["Fantasia"];
+            }?></p>
             <i class="fa-solid fa-pizza-slice"></i>
 
         </div>
 
         <div class="dadosUsuario">
             <br>
-            
-            Nome:<?=$_SESSION["FUNC-NOME"]?><br>
-            E-mail:<?=$_SESSION["FUNC-EMAIL"]?> <br>
-            
+
+            Nome:<?= $_SESSION["FUNC-NOME"] ?><br>
+            E-mail:<?= $_SESSION["FUNC-EMAIL"] ?> <br>
+
             <!-- Cargo:<br> -->
-           
+
         </div>
         <button class="sair" onclick="ExecutaLogout();">Sair</button>
-     </header>
-     <div class="conteiner">
+    </header>
+    <div class="conteiner">
         <div class="menu">
             <a href="admList.php" target="screen">Funcionários</a>
             <a href="entregadorList.php" target="screen">Entregadores</a>
@@ -56,15 +65,17 @@ require_once "admVerifSession.php";
             <a href="categoriaList.php" target="screen">Categorias</a>
             <a href="cargoList.php" target="screen">Cargos</a>
             <a href="empresaEdit.php" target="screen">Empresa</a>
+
         </div>
         <div class="ipres">
-            <iframe name="screen" id="screen" width="100%" height="1180px" src="bemvindo.php" style="border: 0px;"></iframe> 
+            <iframe name="screen" id="screen" width="100%" height="1180px" src="bemvindo.php" style="border: 0px;"></iframe>
         </div>
 
-     </div>
-     
-     <script src="https://kit.fontawesome.com/5bb743cf48.js" crossorigin="anonymous"></script>
-    
-   
+    </div>
+
+    <script src="https://kit.fontawesome.com/5bb743cf48.js" crossorigin="anonymous"></script>
+
+
 </body>
+
 </html>
