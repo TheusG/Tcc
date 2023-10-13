e04afe52ccaecf85cc6144912450e640a6544379
Felipe Cerqueira <133704759+LipeCerqueira@users.noreply.github.com>
2023-10-13 13:29:48 -0300

    Update adm.php

--- adm/view/adm.php
+++ adm/view/adm.php
@@ -25,23 +25,27 @@ require_once "admVerifSession.php";
 </head>
 
 <body>
+
+    <?php
+    require_once "../model/Empresa.class.php";
+    $empresa = new Empresa();
+    $info = $empresa->infoEmpresa();
+
+
+    ?>
     <header>
         <div class="logo">
 
 
+            
+            <img src="../../image/<?php for ($i = 0; $i < count($info); $i++) {
+                    echo $info[$i]["Logo"];
+                } ?>" alt="Logo">
 
-            <img src="img/logo pizzaria1.png" alt="Logo">
 
-            <?php
-            require_once "../model/Empresa.class.php";
-            $empresa = new Empresa();
-            $info = $empresa->infoEmpresa();
- 
-           
-            ?>
-            <p><?php for ($i = 0; $i < count($info); $i++){
-                echo $info[$i]["Fantasia"];
-            }?></p>
+            <p><?php for ($i = 0; $i < count($info); $i++) {
+                    echo $info[$i]["Fantasia"];
+                } ?></p>
             <i class="fa-solid fa-pizza-slice"></i>
 
         </div>
@@ -51,14 +55,14 @@ require_once "admVerifSession.php";
 
             Nome:<?= $_SESSION["FUNC-NOME"]; ?><br>
             E-mail:<?= $_SESSION["FUNC-EMAIL"]; ?> <br>
-            Poder: <?php if($_SESSION["FUNC-PERFIL"] == 1){
-                echo "Total<br>";
-            }else if($_SESSION["FUNC-PERFIL"] == 2){
-                echo "Parcial<br>";
-            }else{
-                echo "Mínimo<br>";
-            }
-            ?>
+            Poder: <?php if ($_SESSION["FUNC-PERFIL"] == 1) {
+                        echo "Total<br>";
+                    } else if ($_SESSION["FUNC-PERFIL"] == 2) {
+                        echo "Parcial<br>";
+                    } else {
+                        echo "Mínimo<br>";
+                    }
+                    ?>
             <!-- Cargo:<br> -->
 
         </div>
@@ -66,27 +70,30 @@ require_once "admVerifSession.php";
     </header>
     <div class="conteiner">
         <div class="menu">
-            <?php 
-                if($_SESSION["FUNC-PERFIL"] == 1){
-                    echo "<a href=\"admList.php\" target=\"screen\">Funcionários</a>";
-                    echo "<a href=\"entregadorList.php\" target=\"screen\">Entregadores</a>";
-                }
-            
+            <?php
+            if ($_SESSION["FUNC-PERFIL"] == 1) {
+                echo "<a href=\"admList.php\" target=\"screen\">Funcionários</a>";
+                echo "<a href=\"entregadorList.php\" target=\"screen\">Entregadores</a>";
+            }
+
             ?>
             <a href="produtoList.php" target="screen">Produtos</a>
             <a href="categoriaList.php" target="screen">Categorias</a>
-            <?php 
-                if($_SESSION["FUNC-PERFIL"] == 1){
-                    echo"<a href=\"cargoList.php\" target=\"screen\">Cargos</a>";
-                    echo "<a href=\"configEdit2.php\" target=\"screen\">Configurações</a>";
-                    echo "<a href=\"empresaEdit2.php\" target=\"screen\">Empresa</a>";
-                }
-            
+            <?php
+            if ($_SESSION["FUNC-PERFIL"] == 1) {
+                echo "<a href=\"cargoList.php\" target=\"screen\">Cargos</a>";
+                echo "<a href=\"configEdit2.php\" target=\"screen\">Configurações</a>";
+                echo "<a href=\"empresaEdit2.php\" target=\"screen\">Empresa</a>";
+            }
+
+            var_dump($info);
             ?>
-            
-          
-            
-            
+
+
+
+
+
+
 
         </div>
         <div class="ipres">
@@ -98,4 +105,5 @@ require_once "admVerifSession.php";
     <script src="https://kit.fontawesome.com/5bb743cf48.js" crossorigin="anonymous"></script>
 
 </body>
+
 </html>
