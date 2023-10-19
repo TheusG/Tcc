18156e112329c31519707f01917300d33487745b
Theus <matheusgomeslucas@gmail.com>
2023-10-18 22:09:17 -0300

    s

--- jsteste.js
+++ jsteste.js
@@ -1,14 +1,25 @@
-const botoesCardapio = document.querySelectorAll('.botaodoscrias');
-const ConteinerProduto = document.querySelector('#idConteinerProduto');
-const quitButton = document.querySelector('#quitButton');
+// const botoesCardapio = document.querySelectorAll('.botaodoscrias');
+// const ConteinerProduto = document.querySelector('#idConteinerProduto');
+// const quitButton = document.querySelector('#quitButton');
 
-botoesCardapio.forEach(botao => {
-  botao.addEventListener('click', () => {
-    ConteinerProduto.classList.toggle('conteinerProdutoOn');
-  });
-});
+// botoesCardapio.forEach(botao => {
+//   botao.addEventListener('click', () => {
+//     ConteinerProduto.classList.toggle('conteinerProdutoOn');
+//   });
+// });
 
-quitButton.addEventListener('click', () => {
-  ConteinerProduto.classList.remove('conteinerProdutoOn');
-  ConteinerProduto.classList.add('conteinerProdutoOf');
+// quitButton.addEventListener('click', () => {
+//   ConteinerProduto.classList.remove('conteinerProdutoOn');
+//   ConteinerProduto.classList.add('conteinerProdutoOf');
+// });
+
+const botaoConfirmarCompra = document.querySelector('#BotaoBolado');
+const miniCarrinho = document.querySelector('.miniCarrinhoOf');
+
+
+
+
+botaoConfirmarCompra.addEventListener('click', () => {
+    miniCarrinho.classList.toggle('miniCarrinhoOn');
+
 });
