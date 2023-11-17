f0f9e2a91f60b5d4b765a2804831641f996b0791
Theus <matheusgomeslucas@gmail.com>
2023-11-17 18:38:20 -0300

    s

--- sobre.php
+++ sobre.php
@@ -72,151 +72,169 @@ session_start();
             <div id="linha3"></div>
         </div>
         <div class="login">
-          <img src="image/semfoto-removebg-preview.png" class="imgMenuCheio" alt="">
-          
-          <?php 
+            <img src="image/semfoto-removebg-preview.png" class="imgMenuCheio" alt="">
 
-            if (isset($_SESSION["LOGADO"]) && $_SESSION["LOGADO"] =! 0){
-              // echo "<a href=\"perfil.php\">".print_r($_SESSION["CLI-EMAIL"]). "</a>";
-              echo "<a href=\"perfil.php\">Convidado</a>";
-              echo  "<button class=\"sair\" onclick=\"ExecutaLogout();\">Sair</button>";
-            }else{
-              echo "<button id=\"abrirLogin\">Login</button>";
+            <?php
+
+            if (isset($_SESSION["LOGADO"]) && $_SESSION["LOGADO"] = !0) {
+                // echo "<a href=\"perfil.php\">".print_r($_SESSION["CLI-EMAIL"]). "</a>";
+                echo "<a href=\"perfil.php\">Convidado</a>";
+                echo  "<button class=\"sair\" onclick=\"ExecutaLogout();\">Sair</button>";
+            } else {
+                echo "<button id=\"abrirLogin\">Login</button>";
             }
-               
-          ?>
-          </div>
+
+            ?>
+        </div>
     </header>
 
 
     <div class="megaConteiner" id="conteinerLogin">
-      <button class="exitButton" id="exitButtonn">X</button>
-      <div class="conteiner">
-        <div class="title">
-            <img src="image/logoPizzaria1.png" alt="">
-            <h1> <i class="fa-solid fa-star"></i>Five Stars <i class="fa-solid fa-star"></i></h1>
+        <button class="exitButton" id="exitButtonn">X</button>
+        <div class="conteiner">
+            <div class="title">
+                <img src="image/logoPizzaria1.png" alt="">
+                <h1> <i class="fa-solid fa-star"></i>Five Stars <i class="fa-solid fa-star"></i></h1>
+            </div>
+            <br><br>
+
+            <div class="conteinerCadastro">
+                <div class="logar">
+
+                    <h2>Entrar</h2>
+
+                    <br>
+                    <form action="controller/controller.php">
+                        <input type="hidden" name="validaCliente" value="1">
+                        <h3>Email</h3>
+                        <input type="email" name="email">
+                        <br><br>
+                        <h3>Senha</h3>
+                        <input type="password" name="senha">
+                        <br><br>
+                        <div class="divButton">
+                            <button> Entrar </button>
+                        </div>
+                    </form>
+
+                </div>
+                <div class="risco">
+
+                </div>
+                <div class="cadastrar">
+
+                    <h2>Cadastrar</h2>
+
+                    <br>
+                    <form action="controller/controller.php" method="post">
+                        <input type="hidden" name="add_cliente" value="">
+                        <h3>Email</h3>
+                        <input type="email" name="email">
+                        <br><br>
+                        <h3>Senha</h3>
+                        <input type="password" name="senha">
+                        <br><br>
+                        <h3>Confirmar senha </h3>
+                        <input type="password" name="confSenha">
+                        <br><br>
+                        <div class="divButton">
+                            <button> Cadastrar </button>
+                        </div>
+                    </form>
+
+
+
+                </div>
+            </div>
         </div>
-        <br><br>
-        
-        <div class="conteinerCadastro">
-            <div class="logar">
-                
-                <h2>Entrar</h2>
-                
-                <br>
-                <form action="controller/controller.php">
-                <input type="hidden" name="validaCliente" value="1">
-                    <h3>Email</h3>
-                    <input type="email" name="email">
-                    <br><br>
-                    <h3>Senha</h3>
-                    <input type="password" name="senha">
-                    <br><br>
-                    <div class="divButton">
-                        <button> Entrar </button>
-                    </div>
-                </form>
-    
-            </div>
-            <div class="risco">
-    
-            </div>
-            <div class="cadastrar">
-                
-                <h2>Cadastrar</h2>
-                
-                <br>
-                <form action="controller/controller.php" method="post">
-                <input type="hidden" name="add_cliente" value="">
-                    <h3>Email</h3>
-                    <input type="email" name="email">
-                    <br><br>
-                    <h3>Senha</h3>
-                    <input type="password" name="senha">
-                    <br><br>
-                    <h3>Confirmar senha </h3>
-                    <input type="password" name="confSenha">
-                    <br><br>
-                    <div class="divButton">
-                        <button>  Cadastrar </button>
-                    </div>
-                </form>
-    
-    
-    
-            </div>
-        </div>
-      </div>
     </div>
 
     <div class="miniCarrinhoOf">
-<?php 
+        <?php
 
-if (isset($_SESSION["LOGADO"]) && $_SESSION["LOGADO"] =! 0){
-?>
-  <div class="divPedido">
-        
-        <div class="divImgPedido">
-            <img src="image/toji.jpg" alt="">
-        </div>
-    
-        <div class="conteinerPedido">
-          
-          <button id="botaoFecharCarrinho">X</button>
-            <div class="divNomeProduto">$Nome da Pizza$</div>
-            <div class="valorPedido"> $Valor$</div>
-            <div class="quantidade">
-            <button><i class="fa-solid fa-minus"></i> </button>
-            <p>0</p>
-            <button><i class="fa-solid fa-plus"></i> </button>
+        if (isset($_SESSION["LOGADO"]) && $_SESSION["LOGADO"] = !0) {
+        ?>
+            <div class="divPedido">
+
+                <div class="divImgPedido">
+                    <img src="image/toji.jpg" alt="">
+                </div>
+
+                <div class="conteinerPedido">
+
+                    <button id="botaoFecharCarrinho">X</button>
+                    <div class="divNomeProduto">$Nome da Pizza$</div>
+                    <div class="valorPedido"> $Valor$</div>
+                    <div class="quantidade">
+                        <button><i class="fa-solid fa-minus"></i> </button>
+                        <p>0</p>
+                        <button><i class="fa-solid fa-plus"></i> </button>
+                    </div>
+
+                </div>
             </div>
-          
-        </div>
+
+            <div class="divValorTotal">
+                <p>Valor Total$</p>
+            </div>
+            <button id="botaoConfirmarCompra">Confirmar compra</button>
+
+        <?php
+        } else {
+            echo "<button id=\"botaoConfirmarCompra\">Carrinho Vazio</button>";
+        }
+        ?>
+
     </div>
-    
-    <div class="divValorTotal">
-          <p>Valor Total$</p>
-      </div>
-    <button id="botaoConfirmarCompra">Confirmar compra</button>
-
-    <?php 
-}else{
-  echo "<button id=\"botaoConfirmarCompra\">Carrinho Vazio</button>";
-
-}
-    ?>
-
-</div>
     <div class="conteinerSobre">
         <div class="ConteinerHistoria">
-        <div>
-            <h2>Fivestars</h2>
-            <br>
-            <p>Fundada em 2023 por 6 estudantes, a Pizzaria Five Stars tem como objetivo que cada mordida seja uma viagem
-            culinária única e memorável. Nossa pizzaria é muito mais do que apenas um restaurante, é um refúgio
-            gastronômico para todos os amantes de uma boas esfihas.</p>
-        </div>
-        <div class="divHistoria">
-            <h2>historia</h2>
-            <br>
-            <p>Fundada em 2023 por 6 estudantes, a Pizzaria Five Stars tem como objetivo que cada mordida seja uma viagem
-            culinária única e memorável. Nossa pizzaria é muito mais do que apenas um restaurante, é um refúgio
-            gastronômico para todos os amantes de uma boa pizza.<br>
-            Nossa variedade de sabores é verdadeiramente vasta e criativa. Desde as clássicas Mussarela e Calabresa até
-            as inovadoras combinações de sabores como a "Vegetariana", além dos nossos talentosos pizzaiolos, que
-            combinam ingredientes frescos e de alta qualidade para oferecer uma experiência única de sabor em cada
-            fatia. Para completar sua experiência, não deixe de experimentar nossas deliciosas e irresistíveis esfihas.</p>
-        </div>
+            <div>
+                <h2>Fivestars</h2>
+                <br>
+                <p>Fundada em 2023 por 6 estudantes, a Pizzaria Five Stars tem como objetivo que cada mordida seja uma viagem
+                    culinária única e memorável. Nossa pizzaria é muito mais do que apenas um restaurante, é um refúgio
+                    gastronômico para todos os amantes de uma boas esfihas.</p>
+            </div>
+            <div class="divHistoria">
+                <h2>historia</h2>
+                <br>
+                <p>Fundada em 2023 por 6 estudantes, a Pizzaria Five Stars tem como objetivo que cada mordida seja uma viagem
+                    culinária única e memorável. Nossa pizzaria é muito mais do que apenas um restaurante, é um refúgio
+                    gastronômico para todos os amantes de uma boa pizza.<br>
+                    Nossa variedade de sabores é verdadeiramente vasta e criativa. Desde as clássicas Mussarela e Calabresa até
+                    as inovadoras combinações de sabores como a "Vegetariana", além dos nossos talentosos pizzaiolos, que
+                    combinam ingredientes frescos e de alta qualidade para oferecer uma experiência única de sabor em cada
+                    fatia. Para completar sua experiência, não deixe de experimentar nossas deliciosas e irresistíveis esfihas.</p>
+            </div>
         </div>
         <div class="conteinerValores">
             <div class="missao">
-
+            <i class="fa-solid fa-bullseye"></i>
+            <br><br>
+            <h3>missao</h3>
+            <br>
+                <p>Fazer valer a experiência do consumidor, através da qualidade e agilidade
+                     de nossos serviços, demonstrando sempre nossos valores e princípios. </p>
             </div>
             <div class="visao">
+                
+                <i class="fa-solid fa-binoculars"></i>
+                <br><br>
+                <h3>visao</h3>
+                <br>
+                <p>Pessoas buscam praticidade, é necessário lhe oferecer o serviço mais prático 
+                    possível, sempre dando lugar a novas ideias.</p>
 
             </div>
             <div class="valores">
-                
+            <i class="fa-solid fa-anchor"></i>
+            <br><br>
+            
+            <h3>valores</h3>
+            <br>
+                <p>Além de valores éticos e morais, nossa empresa visa administrar de forma consciente 
+                    nosso contato com o público</p>
+
             </div>
         </div>
 
@@ -252,7 +270,7 @@ if (isset($_SESSION["LOGADO"]) && $_SESSION["LOGADO"] =! 0){
             <div>
                 <h5> Trabalhe conosco</h5>
                 <a href="">Parceiras</a>
-            </div>  
+            </div>
 
             <div>
                 <h5>Conheça mais</h5>
