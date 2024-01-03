527df7f8ed69d3fe867a36df4534eba6d8fe403a
Theus <matheusgomeslucas@gmail.com>
2024-01-03 15:20:48 -0300

    s

--- dadosCliente2.php
+++ dadosCliente2.php
@@ -93,7 +93,68 @@ session_start();
 
 
 
-   
+   <!-- 
+<div class="conteinerPagamento">
+    <div class="informacoes">
+      
+
+      <div class="nome">
+        <h3> Identificação</h3>
+        <div>
+          <div class="campos">
+            <h4>Nome</h4>
+            <input type="text" name="nome" required value="">
+          </div>
+          <div class="campos">
+            <h4>email</h4>
+            <input type="text" name="nome" required value="">
+          </div>
+          <div class="campos">
+            <h4>Telefone</h4>
+            <input placeholder="(99)99999-9999" id="telefone" name="telefone" type="tel" required value="">
+          </div>
+        </div>
+      </div>
+
+      <div class="endereco">
+        <h3> Endereço</h3>
+        <div>
+          <div class="campos">
+            <h4>CEP</h4>
+            <input type="text" value="">
+          </div>
+          <div class="campos">
+            <h4>Cidade</h4>
+            <input type="text" value="">
+          </div>
+          <div class="campos">
+            <h4>Bairro</h4>
+            <input type="text" value="">
+          </div>
+        </div>
+
+        <div>
+          <div class="campos">
+            <h4>Número</h4>
+            <input type="text" value="">
+          </div>
+          <div class="camposRua">
+            <h4>Rua</h4>
+            <input type="text" value="">
+          </div>
+          <div class="camposRua">
+            <h4>Complemento</h4>
+            <input type="text" value="">
+          </div>
+        </div>
+      </div>
+
+      <button class="botaoSalvar">Salvar</button>
+
+
+
+    </div>
+ -->
 
 
   </div>
